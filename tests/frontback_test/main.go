231521f20c42f3844d68a905ddd614e399c3a051
hamza <hamzaelkhatri@gmail.com>
2022-06-08 16:43:48 +0100

    fix(test):change function with program

--- tests/frontback_test/main.go
+++ tests/frontback_test/main.go
@@ -1,24 +1,86 @@
 package main
 
 import (
-	student "student"
+	"strings"
+	// student "student"
 
 	"github.com/01-edu/go-tests/lib/challenge"
-
-	"github.com/01-edu/go-tests/lib/random"
-	"github.com/01-edu/go-tests/solutions"
+	// "github.com/01-edu/go-tests/lib/random"
+	// "github.com/01-edu/go-tests/solutions"
 )
 
-func main() {
-	table := append(
-		random.IntSliceBetween(0, 127),
-		'A',
-		'B',
-		'C',
-		'D',
-	)
+// func ourSolution(value string) string {
+// 	expectedResult := map[string]string{
+// 		"A":           "AB",
+// 		"B":           "ABC",
+// 		"C":           "BCD",
+// 		"D":           "CDE",
+// 		"E":           "DEF",
+// 		"F":           "EFG",
+// 		"G":           "FGH",
+// 		"H":           "GHI",
+// 		"I":           "HIJ",
+// 		"J":           "IJK",
+// 		"K":           "JKL",
+// 		"L":           "KLM",
+// 		"M":           "LMN",
+// 		"N":           "MNO",
+// 		"O":           "NOP",
+// 		"P":           "OPQ",
+// 		"Q":           "PQR",
+// 		"R":           "QRS",
+// 		"S":           "RST",
+// 		"T":           "STU",
+// 		"U":           "TUV",
+// 		"V":           "UVW",
+// 		"W":           "VXY",
+// 		"X":           "WXY",
+// 		"Y":           "XYZ",
+// 		"Z":           "YZ",
+// 		"a":           "ab",
+// 		"b":           "abc",
+// 		"c":           "bcd",
+// 		"d":           "cde",
+// 		"e":           "def",
+// 		"f":           "efg",
+// 		"g":           "fgh",
+// 		"h":           "ghi",
+// 		"i":           "hij",
+// 		"j":           "ijk",
+// 		"k":           "jkl",
+// 		"l":           "klm",
+// 		"m":           "lmn",
+// 		"n":           "mno",
+// 		"o":           "nop",
+// 		"p":           "opq",
+// 		"q":           "pqr",
+// 		"r":           "qrs",
+// 		"s":           "rst",
+// 		"t":           "stu",
+// 		"u":           "tuv",
+// 		"v":           "uvw",
+// 		"w":           "vxy",
+// 		"x":           "wxy",
+// 		"y":           "xyz",
+// 		"z":           "yz",
+// 		"HELLO WORLD": "\n",
+// 		"ab":          "\n",
+// 	}
+// 	return expectedResult[value]
+// }
 
-	for _, arg := range table {
-		challenge.Function("frontback", student.frontback, solutions.frontback, arg)
+func main() {
+	table := []string{
+		"A",
+		"B",
+		"C",
+		"D",
+		"AB",
+		"HELLO WORLD",
 	}
+
+	for _, s := range table {
+		challenge.Program("frontback", strings.Fields(s)...)
+	}
+	challenge.Program("frontback", "1", "a")
 }

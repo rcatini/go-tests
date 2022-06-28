7c188517b9d51fd931b9eddb43bc42212984543a
estlop <lopezesteban8@gmail.com>
2022-06-28 13:12:54 +0100

    feat: add solution for sumascii subject

---
+++ solutions/sumascii/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println(0)
+	}
+	var result byte
+	for _, byte := range []byte(os.Args[1]) {
+		result += byte
+	}
+	fmt.Println(result)
+}

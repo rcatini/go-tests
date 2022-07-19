741dcae445de9cfd25d482e786b003c88bdbb367
Hamza elkhatri <40549481+Hamzaelkhatri@users.noreply.github.com>
2022-07-19 18:05:28 +0100

    remove comment

--- tests/frontback_test/main.go
+++ tests/frontback_test/main.go
@@ -7,19 +7,18 @@ import (
 
 func main() {
 	table := []string{
-		"A", // A ---> AB
-		"B", // B ---> ABC
-		"C", // C --- BCD
-		"D", // D ---> CDE
-		"AB", // AB ---> \n
-		"a", // a ---> ab
-		"z", // z ---> yz
-		"HELLO WORLD", // \n
-		" ",// \n
+		"A",
+		"B",
+		"C",
+		"D",
+		"AB",
+		"a",
+		"z",
+		"HELLO WORLD",
+		" ",
+		"",
 	}
-
 	for _, s := range table {
 		challenge.Program("frontback", strings.Fields(s)...)
 	}
-	challenge.Program("frontback", "1")
 }

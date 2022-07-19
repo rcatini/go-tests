6270d808269f0df29e9d9586e832a074a46cfc9f
zainabdnaya <diyanazizo13@gmail.com>
2022-07-20 00:00:50 +0100

    corrected format + add tests

--- solutions/weareunique.go
+++ solutions/weareunique.go
@@ -3,23 +3,22 @@ package solutions
 import "strings"
 
 func Weareunique(str1 string, str2 string) int {
-
-	var	used[127] int 
+	var used [127]int
 	var argv []string = []string{str1, str2}
 	k := 0
 	i := 0
-	for (i < 127){
+	for i < 127 {
 		used[i] = 0
 		i++
 	}
 	i = 0
-	for (i < 2){
+	for i < 2 {
 		j := 0
-		for j < len(argv[i]){
+		for j < len(argv[i]) {
 			if used[argv[i][j]] == 0 && !strings.Contains(argv[1-i], string(argv[i][j])) {
-                used[argv[i][j]] = 1
-                k++
-            }
+				used[argv[i][j]] = 1
+				k++
+			}
 			j++
 		}
 		i++
--- tests/weareunique_test/main.go
+++ tests/weareunique_test/main.go
@@ -9,13 +9,19 @@ import (
 
 func main() {
 	table := [][]string{
-		{"foo", "boo"},
-		{"abc", "def"},
-		{"hello", "yoall"},
-		{"everyone", ""},
-		{"hello world", "fam"},
 		{"", ""},
 		{"abc", "abc"},
+		{"foo", "boo"},
+		{"abc", "def"},
+		{"+265", "265"},
+		{"eveRyone", ""},
+		{"HellO", "yoAll"},
+		{"123231", "123231"},
+		{"w^p@@j", "w^p@@j"},
+		{"26235e5", "4478q92"},
+		{"hello woRld", "fam"},
+		{"		", "		 "},
+		{"AB$%d.52", "eepqdl.52"},
 	}
 	for _, arg := range table {
 		challenge.Function("Weareunique", student.Weareunique, solutions.Weareunique, arg)
